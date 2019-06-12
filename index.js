4b9ac5756d3109786c3d84d1ddd0091b72dc2088
RoyalTux <dmitry.shebetovsky@gmail.com>
2019-06-12 22:10:09 +0300

    Added async

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Star Wars</h1>`;
 var div = document.getElementById('list');
@@ -10,75 +8,127 @@ div.appendChild(ul);
 
 showMainInfo();
 
-function getJSON(url) 
-{
-  var resp = '';
+function showMainInfo(){
   var xhr = new XMLHttpRequest();
-  if (xhr !== null) 
-  {
-    xhr.open("GET", url, false);
-    xhr.send();
-    resp = xhr.responseText;
-  }
-  return resp;
+  xhr.open('GET', 'https://swapi.co/api/films/');
+  xhr.send();
+
+  xhr.onload = function() {
+  if (xhr.status != 200) {
+    console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+  } 
+  else {
+    console.log(`Done, got ${xhr.response.length} bytes`);
+    var parsed = JSON.parse(xhr.responseText);
+    for (var i = 0; i <= parsed.results.length - 1; i++) {
+      var li = document.createElement('li');
+
+      li.innerHTML = '<i>Name: </i>' + 
+        '<a id ="' + parsed.results[i].url + '"href="#">' + parsed.results[i].title + '</a>' + '<br/>' +
+          '<i>Episode number: </i>' + parsed.results[i].episode_id + '<br/>' +
+          '<i>Description: </i>' + parsed.results[i].opening_crawl + '<br/>' +
+          '<i>Director: </i>' + parsed.results[i].director + '<br/>' +
+          '<i>Producer: </i>' + parsed.results[i].producer + '<br/>' +
+          '<i>Release Date: </i>' + parsed.results[i].release_date + '<br/><br/><hr></hr><br/>';
+      ul.appendChild(li);
+
+      (function (index) {
+          li.onclick = function() { moreInfo(parsed.results[index].url) }
+        }(i));
+
+      }
+    }
+  };
+
+  xhr.onprogress = function(event) {
+    if (event.lengthComputable) {
+      console.log(`Received ${event.loaded} of ${event.total} bytes`);
+    }
+    else{
+      console.log(`Received ${event.loaded} bytes`);
+    }
+  };
+
+  xhr.onerror = function() {
+    console.log("Request failed");
+  };
 }
 
-function showMainInfo()
-{
-  var json = getJSON('https://swapi.co/api/films/');
-  var parsed = JSON.parse(json);
-  for (var i = 0; i <= parsed.results.length - 1; i++) 
-  {
-    var li = document.createElement('li');
-  
-    li.innerHTML = '<i>Name: </i>' + 
-    '<a id ="' + parsed.results[i].url + '"href="#">' + parsed.results[i].title + '</a>' + '<br/>' +
-      '<i>Episode number: </i>' + parsed.results[i].episode_id + '<br/>' +
-      '<i>Description: </i>' + parsed.results[i].opening_crawl + '<br/>' +
-      '<i>Director: </i>' + parsed.results[i].director + '<br/>' +
-      '<i>Producer: </i>' + parsed.results[i].producer + '<br/>' +
-      '<i>Release Date: </i>' + parsed.results[i].release_date + '<br/><br/>';
-    ul.appendChild(li);
+function moreInfo(url){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send();
 
-    (function (index) {
-      li.onclick = function() { moreInfo(parsed.results[index].url) }
-    }(i));
-  }
-}
+  xhr.onload = function() {
+    if (xhr.status != 200) {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    } 
+    else {
+      console.log(`Done, got ${xhr.response.length} bytes`);
+      var parsed = JSON.parse(xhr.responseText);
+      var leftContent = document.getElementById("leftContent");
+      leftContent.innerHTML = "";
+      var rightContent = document.getElementById("rightContent");
+      rightContent.innerHTML = "";
 
-function moreInfo(url)
-{
-  var json = getJSON(url); 
-  var parsed = JSON.parse(json);
-  var leftContent = document.getElementById("leftContent");
-  leftContent.innerHTML = "";
-  var rightContent = document.getElementById("rightContent");
-  rightContent.innerHTML = "";
-  for (var i = 0; i <= parsed.characters.length - 1; i++)
-  {
-    var peopleJSON = getJSON(parsed.characters[i]);
-    var people = JSON.parse(peopleJSON);
-    //console.log(people.name);
-    //console.log(people.gender);
-    leftContent.innerHTML += "<h4>" + people.name + "</h4>";
-    rightContent.innerHTML += "<h4>" + people.gender + "</h4>";
+      for (var i = 0; i <= parsed.characters.length - 1; i++) {    
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', parsed.characters[i]);
+        xhr.send();
 
-  }
+        xhr.onload = function() {
+          if (xhr.status != 200) {
+          console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+          } 
+          else {
+            console.log(`Done, got ${xhr.response.length} bytes`);
+            let people = JSON.parse(xhr.responseText);
+            leftContent.innerHTML += "<h4>" + people.name + "</h4>";
+            rightContent.innerHTML += "<h4>" + people.gender + "</h4>";
+          }
+        };
+
+        xhr.onprogress = function(event) {
+          if (event.lengthComputable) {
+            console.log(`Received ${event.loaded} of ${event.total} bytes`);
+          }
+          else{
+            console.log(`Received ${event.loaded} bytes`);
+          }
+        };
+
+        xhr.onerror = function() {
+          console.log("Request failed");
+        };
+      }
+    }
+  };
+
   showModalWin();
-}
- 
 
-function showModalWin() 
-{
-  var darkLayer = document.createElement('div'); // слой затемнения
-  darkLayer.id = 'shadow'; // id чтобы подхватить стиль
-  document.body.appendChild(darkLayer); // включаем затемнение
-  var modalWin = document.getElementById('popupWin'); // находим наше "окно"
-  modalWin.style.display = 'block'; // "включаем" его
-  darkLayer.onclick = function () // при клике на слой затемнения все исчезнет
-  {  
-    darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
-    modalWin.style.display = 'none'; // делаем окно невидимым
+  xhr.onprogress = function(event) {
+    if (event.lengthComputable) {
+      console.log(`Received ${event.loaded} of ${event.total} bytes`);
+    }
+    else{
+      console.log(`Received ${event.loaded} bytes`);
+    }
+  };
+  
+  xhr.onerror = function() {
+    console.log("Request failed");
+  };
+}
+
+function showModalWin() {
+  var darkLayer = document.createElement('div'); 
+  darkLayer.id = 'shadow'; 
+  document.body.appendChild(darkLayer);
+  var modalWin = document.getElementById('popupWin'); 
+  modalWin.style.display = 'block'; 
+  darkLayer.onclick = function (){  
+    darkLayer.parentNode.removeChild(darkLayer); 
+    modalWin.style.display = 'none'; 
     return false;
   };
 }
